8a276333677edbdaf03486e1a7fa79600e83d773
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-07-14 22:47:44 +0530

    new file:   .gitignore

---
+++ app/(marketing)/(landing-page)/page.tsx
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gay-y-[8px]">
+        LandingPage
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import LandingPage from "./(landing-page)/page";
+import Navbar from "./navbar/navbar";
+
+const MarketingPage = () => { 
+  return (
+    <div>
+      <Navbar />
+      <LandingPage />
+    </div>
+  );
+}
+
+export default MarketingPage;
