e28fea2f0a4b2774428add904fda9c49d1396b40
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-07-19 17:23:11 +0530

    modified:   app/(marketing)/(landing-page)/_components/hero.tsx

--- app/(marketing)/(landing-page)/_components/hero.tsx
+++ app/(marketing)/(landing-page)/_components/hero.tsx
@@ -1,6 +1,26 @@
+import { BsFillArrowLeftSquareFill, BsFillArrowRightSquareFill } from "react-icons/bs";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="h-300 relative bg-[#fff] w-full">
+      <div>
+        <div>
+          <h3>Best Offers</h3>
+        </div>
+        <div>
+          <BsFillArrowLeftSquareFill />
+          <BsFillArrowRightSquareFill />
+        </div>
+      </div>
+      <div className="relative w-[1000px] mt-[150px] mx-auto h-120 grid grid-cols-3 gap-[40px]">
+        <div className="relative bg-[#000] overflow-hidden">
+          <div>img</div>
+          <div>content</div>
+        </div>
+        <div className="relative bg-[#000] overflow-hidden">card</div>
+        <div className="relative bg-[#000] overflow-hidden">card</div>
+      </div>
+    </div>
   );
 };
 
