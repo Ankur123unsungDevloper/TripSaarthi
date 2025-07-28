bcc4681fda2c2e8ba24da1ea6495f986c061ba22
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-07-29 01:22:38 +0530

    modified:   app/(marketing)/(landing-page)/_components/ignoretools.tsx 	modified:   app/(marketing)/(landing-page)/_components/partner.tsx 	modified:   public/logo/bookingdotcom-logo.svg

--- app/(marketing)/(landing-page)/_components/ignoretools.tsx
+++ app/(marketing)/(landing-page)/_components/ignoretools.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 const IgnoreTools = () => {
   return (
-    <div>
-      <h1>Ignore Tools</h1>
+    <div className="flex flex-row items-center justify-between py-16">
+      <div>
+        <Image
+          src="/images/ignore-tools.png"
+          alt="Ignore Tools"
+          width={300}
+          height={300}
+          className="mx-auto"
+        />
+      </div>
+      <div>
+        <div>
+          <h3>No need to juggle many apps anymore - plan everything in one place.</h3>
+          <p>From flight reservations to activities and accommodation, streamline details on your travel plans into a single, intuitive platform. Say goodbye to scattered bookings and hello to seamless journey management.</p>
+        </div>
+      </div>
     </div>
   );
 };
