c5859522c0afd7b348fc722f3c321c68f2d18da9
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-08-07 16:55:30 +0530

    modified:   app/(marketing)/(landing-page)/_components/ignoretools.tsx

--- app/(marketing)/(landing-page)/_components/ignoretools.tsx
+++ app/(marketing)/(landing-page)/_components/ignoretools.tsx
@@ -1,22 +1,20 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 const IgnoreTools = () => {
   return (
-    <div className="flex flex-row items-center justify-between py-16">
-      <div>
-        <Image
-          src="/images/ignore-tools.png"
+    <div className="flex flex-row items-center justify-between py-16 px-6 w-full">
+      <div className="flex flex-col items-center justify-center h-100 w-450">
+        {/* <Image
+          src="/backgroundImage/Heading_bg.jpg"
           alt="Ignore Tools"
-          width={300}
+          width={1000}
           height={300}
-          className="mx-auto"
-        />
+          className="mx-auto w-2000"
+        /> */}
       </div>
-      <div>
-        <div>
-          <h3>No need to juggle many apps anymore - plan everything in one place.</h3>
-          <p>From flight reservations to activities and accommodation, streamline details on your travel plans into a single, intuitive platform. Say goodbye to scattered bookings and hello to seamless journey management.</p>
-        </div>
+      <div className="flex flex-col items-start justify-center w-full p-4">
+        <h3 className="text-3xl sm:text-3xl md:text-4xl font-bold">No need to juggle many apps anymore - plan everything in one place.</h3>
+        <p className="text-base sm:text-xl md:text-xl font-medium">From flight reservations to activities and accommodation, streamline details on your travel plans into a single, intuitive platform. Say goodbye to scattered bookings and hello to seamless journey management.</p>
       </div>
     </div>
   );
